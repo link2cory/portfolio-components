ee58a85e98c5e823ef30a433b32538102f5ccaf1
Cory Perkins <link2cory@gmail.com>
2020-06-22 14:50:50 -0700

    moved NavBar into organisms subdir

--- src/components/organisms/NavBar/NavBar.tsx
+++ src/components/organisms/NavBar/NavBar.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import Button from '../../atoms/Button'
 import { NavBarProps } from './interfaces'
 
 const StyledNavBar = styled.header`
@@ -33,22 +34,6 @@ const Link = styled.a`
   transition: all 0.3s ease 0s;
   &:hover {
     color: #0088a9;
-  }
-`
-const Button = styled.a`
-  font-weight: 500;
-  font-size: 1rem;
-  color: #edf0f1;
-  text-decoration: none;
-  padding: 9px 25px;
-  background-color: rgba(0, 136, 169, 1);
-  border: none;
-  border-radius: 50px;
-  cursor: pointer;
-  transition: all 0.3s ease 0s;
-
-  &:hover {
-    background-color: rgba(0, 136, 169, 0.8);
   }
 `
 
